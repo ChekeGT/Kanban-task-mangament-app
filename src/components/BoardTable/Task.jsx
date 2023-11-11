9583bdaea2f77cb8c9a8c5dc78031feb8c7cfd7d
Gavbriel015 <av.gabriel015@gmail.com>
2023-11-10 21:57:46 -0400

    Added darkmode to Task Component

--- src/components/BoardTable/Task.jsx
+++ src/components/BoardTable/Task.jsx
@@ -1,7 +1,7 @@
 export default function Task({titleTask, completedSubtasks}) {
     return(
-        <div className="hover:text-mainPurple cursor-pointer max-w-[280px] shadow-md flex flex-col gap-1 p-3 pr-12 rounded-lg items-start">
-            <h2 className="font-semibold">Build UI for onboarding flow</h2>
+        <div className="dark:bg-darkGrey hover:text-mainPurple cursor-pointer max-w-[280px] shadow-md flex flex-col gap-1 p-3 pr-12 rounded-lg items-start">
+            <h2 className="dark:text-white font-semibold">Build UI for onboarding flow</h2>
             <p className="text-sm text-grayText">0 of 3 subtasks</p>
         </div>
     )
