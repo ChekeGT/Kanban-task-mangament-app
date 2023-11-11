e3b680039e195d08571869ccdf704e80fa5590f2
Gavbriel015 <av.gabriel015@gmail.com>
2023-11-10 21:39:32 -0400

    Added darkmode to boardtemplate component

--- src/components/PopUps/BoardTemplate.jsx
+++ src/components/PopUps/BoardTemplate.jsx
@@ -7,23 +7,23 @@ export default function BoardTemplate() {
     }
     
     return(
-        <div onClick={preventPropagation} className="w-[480px] bg-white px-6 py-8 flex flex-col gap-5">
-            <h1 className="font-bold text-xl ">Add New Board</h1>
+        <div onClick={preventPropagation} className="dark:bg-darkGrey w-[480px] bg-white px-6 py-8 flex flex-col gap-5">
+            <h1 className="dark:text-white font-bold text-xl ">Add New Board</h1>
             <div className="flex flex-col gap-2">
-                <h3 className="font-bold text-grayText">Name</h3>
-                <input className="w-full p-2 rounded-md  border border-gray" placeholder="e.g. Web Design" type="text" />
+                <h3 className="dark:text-white font-bold text-grayText">Name</h3>
+                <input className="dark:bg-darkGrey w-full p-2 rounded-md  border border-gray" placeholder="e.g. Web Design" type="text" />
             </div>
-            <div>
-                <h3 className="font-bold text-grayText">Columns</h3>
+            <div className="flex flex-col gap-4">
+                <h3 className="dark:text-white font-bold text-grayText">Columns</h3>
                 <div className="pb-6">
                     {/* COMPONENT COLUMNS */}
                     <div className="flex items-center gap-4">
-                        <input placeholder="e.g. Make Coffe" className="w-full p-2 rounded-md  border border-gray" type="text" />
+                        <input placeholder="e.g. Make Coffe" className="dark:bg-darkGrey w-full p-2 rounded-md  border border-gray" type="text" />
                         <img src={crossIcon} alt="" />
                     </div>
                 </div>
                 <div className="flex flex-col gap-4">
-                    <button className="bg-mainPurple bg-opacity-10 p-2 text-mainPurple font-bold rounded-full">+ Add New Column</button>
+                    <button className="dark:bg-white bg-mainPurple bg-opacity-10 p-2 text-mainPurple font-bold rounded-full">+ Add New Column</button>
                     <button className="bg-mainPurple p-2 rounded-full text-white font-semibold">Create New Board</button>
                 </div>
             </div>
