d74cfeb64b4cb0b4630de958543084e59d15269f
Cheke <chekelosos@gmail.com>
2023-11-11 12:36:29 -0600

    Added a way to autoDestroy a popup when submitted.

--- src/components/PopUps/PopUpContainer.jsx
+++ src/components/PopUps/PopUpContainer.jsx
@@ -1,10 +1,21 @@
+import { createContext, useContext } from "react"
+
+
+let autoDestructionContext = createContext()
+
+
+export function useAutoDestruction(){
+    return useContext(autoDestructionContext)
+}
 export default function PopUpContainer({ children, autoDestructionFunction}){
 
     return (
         // The function will only work if the user clicks in some area that is not the children of this function.
         // why? because on the children of this function it exists a stop propagation function.
-        <div  onClick={autoDestructionFunction} className="bg-blackCover absolute w-11 h-12 left-[50%] translate-x-[-50%] top-[50%] translate-y-[-50%] w-[100%] h-[100%] flex justify-center items-center z-50">
-            {children}
-        </div>
+        <autoDestructionContext.Provider value={autoDestructionFunction}>
+            <div  onClick={autoDestructionFunction} className="bg-blackCover absolute w-11 h-12 left-[50%] translate-x-[-50%] top-[50%] translate-y-[-50%] w-[100%] h-[100%] flex justify-center items-center z-50">
+                {children}
+            </div>
+        </autoDestructionContext.Provider>
     )
 }
