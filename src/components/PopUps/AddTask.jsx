3ef82b980a3a6048af0a49fc7310e1f25343d167
Gavbriel015 <av.gabriel015@gmail.com>
2023-11-10 21:34:49 -0400

    Added Dark mode to AddTask Component

--- src/components/PopUps/AddTask.jsx
+++ src/components/PopUps/AddTask.jsx
@@ -13,32 +13,32 @@ export default function AddTask() {
     }
     
     return(
-        <div onClick={preventPropagation} className="w-[480px] bg-white px-6 py-8 flex flex-col gap-5">
-            <h1 className="font-bold text-xl ">Add New Task</h1>
+        <div onClick={preventPropagation} className="dark:bg-darkGrey w-[480px] bg-white px-6 py-8 flex flex-col gap-5">
+            <h1 className="font-bold text-xl">Add New Task</h1>
             <div className="flex flex-col gap-2">
-                <h3 className="font-bold text-grayText">Title</h3>
-                <input className="w-full p-2 rounded-md  border border-gray" placeholder="e.g. Take coffee break" type="text" />
+                <h3 className="dark:text-white font-bold text-grayText">Title</h3>
+                <input className="dark:bg-darkGrey w-full p-2 rounded-md  border border-gray" placeholder="e.g. Take coffee break" type="text" />
             </div>
             <div>
-                 <h3 className="font-bold text-grayText">Description</h3>
-                <textarea className="resize-none w-full h-[100px] p-2 rounded-md border border-gray" placeholder="e.g. It’s always good to take a break. This 15 minute break will 
+                 <h3 className="dark:text-white font-bold text-grayText">Description</h3>
+                <textarea className="dark:bg-darkGrey resize-none w-full h-[100px] p-2 rounded-md border border-gray" placeholder="e.g. It’s always good to take a break. This 15 minute break will 
                 recharge the batteries a little." type="textarea" />
             </div>
             <div className="flex flex-col gap-2">
-                <h3 className="font-bold text-grayText">Subtasks</h3>
+                <h3 className="dark:text-white font-bold text-grayText">Subtasks</h3>
                 <div className="flex items-center gap-4">
-                    <input placeholder="e.g. Make Coffe" className="w-full p-2 rounded-md  border border-gray" type="text" />
+                    <input placeholder="e.g. Make Coffe" className=" dark:bg-darkGrey w-full p-2 rounded-md  border border-gray" type="text" />
                     <img src={crossIcon} alt="" />
                 </div>
                 <div className="flex items-center gap-4">
-                    <input placeholder="e.g. Drink coffe & smile" className="w-full p-2 rounded-md  border border-gray" type="text" />
+                    <input placeholder="e.g. Drink coffe & smile" className="dark:bg-darkGrey w-full p-2 rounded-md  border border-gray" type="text" />
                     <img src={crossIcon} alt="" />
                 </div>
-                <button className="bg-mainPurple bg-opacity-10 p-2 text-mainPurple font-bold rounded-full">+ Add New Subtask</button>
+                <button className="dark:bg-white bg-mainPurple bg-opacity-10 p-2 text-mainPurple font-bold rounded-full">+ Add New Subtask</button>
             </div>
             <div className="flex flex-col gap-2">
-                <h3 className="font-bold text-grayText">Status</h3>
-                <select className="w-full p-2 rounded-md  border border-gray" name="" id="">
+                <h3 className="dark:text-white font-bold text-grayText">Status</h3>
+                <select  className="dark:bg-darkGrey w-full p-2 rounded-md  border border-gray" name="" id="">
                     <option value="">Todo</option>
                 </select>
             </div>
