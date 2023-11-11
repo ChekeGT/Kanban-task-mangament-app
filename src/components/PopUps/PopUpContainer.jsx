87087fa1e2224b2776e3aa47e3eee617e86e00ab
Cheke <chekelosos@gmail.com>
2023-11-10 22:18:36 -0600

    Fixed a bug with the z-index of the popupcontainer.

--- src/components/PopUps/PopUpContainer.jsx
+++ src/components/PopUps/PopUpContainer.jsx
@@ -3,7 +3,7 @@ export default function PopUpContainer({ children, autoDestructionFunction}){
     return (
         // The function will only work if the user clicks in some area that is not the children of this function.
         // why? because on the children of this function it exists a stop propagation function.
-        <div  onClick={autoDestructionFunction} className="bg-blackCover absolute w-11 h-12 left-[50%] translate-x-[-50%] top-[50%] translate-y-[-50%] w-[100%] h-[100%] flex justify-center items-center">
+        <div  onClick={autoDestructionFunction} className="bg-blackCover absolute w-11 h-12 left-[50%] translate-x-[-50%] top-[50%] translate-y-[-50%] w-[100%] h-[100%] flex justify-center items-center z-50">
             {children}
         </div>
     )
