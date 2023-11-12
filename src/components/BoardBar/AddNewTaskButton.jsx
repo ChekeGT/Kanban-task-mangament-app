eae8aa6ec99165ccab6f36165b924b81da88b52f
Cheke <chekelosos@gmail.com>
2023-11-11 23:10:36 -0600

    Fixed a mistake with the positioning of the add Task popup. Since we recently made the boardBar absolute positioned, it caused trouble with the popup, fixed it by putting the popup aside from the boardBar container.

--- src/components/BoardBar/AddNewTaskButton.jsx
+++ src/components/BoardBar/AddNewTaskButton.jsx
@@ -4,34 +4,10 @@ import { useState } from "react"
 import PopUpContainer from "../PopUps/PopUpContainer"
 import AddTask from '../PopUps/AddTask'
 
-export default function AddNewTaskButton({board}){
-
-    let [showPopUp, setShowPopUp ] = useState(false)
-
-    const handleShowPopUp = () => {
-        setShowPopUp(true)
-    }
-
-    const deletePopUp = () => {
-        setShowPopUp(false)
-    }
-
-    if (showPopUp){
-        return (
-            <>
-                <PopUpContainer autoDestructionFunction={deletePopUp}>
-                    <AddTask/>
-                </PopUpContainer>
-                <button className=" bg-mainPurple text-white p-2 rounded-2xl h-[48px] w-[164px] text-center">
-                    + Add New Task
-                </button>
-            </>
+export default function AddNewTaskButton({handleShowPopUp}){
+    return (
+        <button onClick={handleShowPopUp} className=" bg-mainPurple text-white p-2 rounded-2xl h-[48px] w-[164px] text-center">
+            + Add New Task
+        </button>
         )
-    }else{
-        return (
-            <button onClick={handleShowPopUp} className=" bg-mainPurple text-white p-2 rounded-2xl h-[48px] w-[164px] text-center">
-                + Add New Task
-            </button>
-        )
-    }
 }
--- src/components/BoardBar/BoardBar.jsx
+++ src/components/BoardBar/BoardBar.jsx
@@ -1,14 +1,46 @@
+import { useState } from "react"
 import AddNewTaskButton from "./AddNewTaskButton"
 import HamburgerButton from "./HamburgerButton"
+import AddTask from "../PopUps/AddTask"
+import PopUpContainer from "../PopUps/PopUpContainer"
 
 export default function BoardBar({board}){
-    return (
-        <div className=" flex flex-row items-center justify-between w-10/12 right-0 bg-white shadow  py-7 pl-2 pr-5 h-[96px] dark:bg-darkGrey dark:text-white absolute">
-            <p className="font-custom text-black font-bold text-3xl dark:text-white">{board == null ? 'You have no boards' : board.name}</p>
-            <div className="flex flex-row items-center gap-5">
-                <AddNewTaskButton board={board}/>
-                <HamburgerButton/>
+
+    let [showPopUp, setShowPopUp ] = useState(false)
+
+    const handleShowPopUp = () => {
+        setShowPopUp(true)
+    }
+
+    const deletePopUp = () => {
+        setShowPopUp(false)
+    }
+
+    if (showPopUp){
+        return (
+            <>
+                <div className=" flex flex-row items-center justify-between w-10/12 right-0 bg-white shadow  py-7 pl-2 pr-5 h-[96px] dark:bg-darkGrey dark:text-white absolute">
+                    <p className="font-custom text-black font-bold text-3xl dark:text-white">{board == null ? 'You have no boards' : board.name}</p>
+                    <div className="flex flex-row items-center gap-5">
+                        <AddNewTaskButton board={board} handleShowPopUp={handleShowPopUp}/>
+                        <HamburgerButton/>
+                    </div>
+                </div>
+                <PopUpContainer autoDestructionFunction={deletePopUp}>
+                    <AddTask/>        
+                </PopUpContainer>
+                
+            </>
+        )
+    }else{
+        return (
+            <div className=" flex flex-row items-center justify-between w-10/12 right-0 bg-white shadow  py-7 pl-2 pr-5 h-[96px] dark:bg-darkGrey dark:text-white absolute">
+                <p className="font-custom text-black font-bold text-3xl dark:text-white">{board == null ? 'You have no boards' : board.name}</p>
+                <div className="flex flex-row items-center gap-5">
+                    <AddNewTaskButton board={board} handleShowPopUp={handleShowPopUp}/>
+                    <HamburgerButton/>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
