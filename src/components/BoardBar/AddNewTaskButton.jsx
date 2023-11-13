6a5a41ca783d7410aa18fe9708e207e65a67b5fb
Cheke <chekelosos@gmail.com>
2023-11-13 15:16:06 -0600

    Added functionality to disable the AddNewTaskButton when you have no board selected.

--- src/components/BoardBar/AddNewTaskButton.jsx
+++ src/components/BoardBar/AddNewTaskButton.jsx
@@ -1,10 +1,22 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 // Components
 
-export default function AddNewTaskButton({handleShowPopUp}){
+export default function AddNewTaskButton({showPopUp, board}){
+
+    const [active, setActiveState ] = useState(true)
+
+    function hanldeClick(){
+        if (active){
+            showPopUp()
+        }
+    }
+
+    useEffect(() => {
+        setActiveState(board == null ? false : true)
+    }, [board])
     return (
-        <button onClick={handleShowPopUp} className=" bg-mainPurple text-white p-2 rounded-2xl h-[48px] w-[164px] text-center">
+        <button onClick={hanldeClick} className={` bg-mainPurple text-white p-2 rounded-2xl h-[48px] w-[164px] text-center ${active ? 'opacity-100' : 'opacity-50 cursor-default' }`}>
             + Add New Task
         </button>
         )
--- src/components/BoardBar/BoardBar.jsx
+++ src/components/BoardBar/BoardBar.jsx
@@ -32,7 +32,7 @@ export default function BoardBar({board, numberOfBoards}){
             <div className=" flex flex-row items-center justify-between w-10/12 right-0 bg-white shadow  py-7 pl-2 pr-5 h-[96px] dark:bg-darkGrey dark:text-white absolute">
                 <p className="font-custom text-black font-bold text-3xl dark:text-white">{board == null ? numberOfBoards > 0 ? 'You have no boards selected.' : 'You have no boards' : board.name}</p>
                 <div className="flex flex-row items-center gap-5">
-                     <AddNewTaskButton board={board} handleShowPopUp={toggleShowAddPopUp}/>
+                     <AddNewTaskButton board={board} showPopUp={toggleShowAddPopUp}/>
                     <HamburgerButton type={'Board'} handleShowDeletePopUp={toggleShowDeletePopUp} handleShowEditPopUp={toggleShowEditPopUp}/>
                 </div>
             </div>
