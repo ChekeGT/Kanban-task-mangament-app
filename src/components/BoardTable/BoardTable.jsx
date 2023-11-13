590d0bc032824d45c7e3422e6804a4cbca28e8e2
Gavbriel015 <av.gabriel015@gmail.com>
2023-11-13 16:50:37 -0400

    added white screen when dashboard has no columns

--- src/components/BoardTable/BoardTable.jsx
+++ src/components/BoardTable/BoardTable.jsx
@@ -1,21 +1,30 @@
 import ColumnTask from "./ColumnTasks";
 
 
-export default function BoardTable({ boards }) {
-    if (!boards || !boards.columns) {
-      return (
-        <div className="flex gap-8 pt-4 pl-6 pt-[120px] h-[100%] min-h-screen justify-center items-center">
-          <div className="flex gap-4 flex-col items-center">
-            <h1 className="text-grayText font-bold">
-              This board is empty. Create a new column to get started.
-            </h1>
-            <button className="bg-mainPurple p-2 px-4 rounded-full text-white font-semibold ">
-              + Add New Column
-            </button>
-          </div>
+export default function BoardTable({ boards, numberOfBoards }) {
+    
+  if(numberOfBoards.length === 0){
+    return(
+      <>
+      </>
+    )
+  }
+  
+  if (!boards || !boards.columns || boards.columns.length === 0) {
+    return (
+      <div className="flex gap-8 pt-4 pl-6 pt-[120px] h-[100%] min-h-screen justify-center items-center">
+        <div className="flex gap-4 flex-col items-center">
+          <h1 className="text-grayText font-bold">
+            This board is empty. Create a new column to get started.
+          </h1>
+          <button className="bg-mainPurple p-2 px-4 rounded-full text-white font-semibold">
+            + Add New Column
+          </button>
         </div>
-      );
-    }
+      </div>
+    );
+  }
+    
   
     const columns = boards.columns;
   
