62e02b472f6dc4cdc4f353ac29ec5c81f6fc6745
Cheke <chekelosos@gmail.com>
2023-11-13 16:09:30 -0600

    Added functionality on the reducer to add a new task to the store.

--- src/state_management/reducer.js
+++ src/state_management/reducer.js
@@ -39,6 +39,39 @@ export default function reducer(state, action){
             newState = {...state, boards: newBoards, currentBoard: null}
             localStorage.state = JSON.stringify(newState)
             return newState
+        case ACTIONS.addTask:
+            newBoards = boards.map((board) => {
+                if (board.name == action.payload.board){
+                    board.columns = board.columns.map((column) => {
+                        if (column.name == action.payload.column){
+                            let newTask = {
+                                title: action.payload.name,
+                                description: action.payload.description,
+                                subtasks: action.payload.subTasks.map((subTaskName) => ({title: subTaskName, isCompleted: false}))
+                            }
+                            // This is to make the flux function pure, that means same input always gives the same output.
+                            // No matter what, so we will guarantee by this way we are only adding the newTask once, cause
+                            // react calls this function several times while on Strict mode
+                            let found = false
+                            for (let i = 0; i < column.tasks.length; i++){
+                                let task = column.tasks[i]
+                                if (task.title == newTask.title){
+                                    found = true
+                                }
+                            }
+
+                            if (!found){
+                                column.tasks.push(newTask)
+                            }
+                        }
+                        return column
+                    })
+                }
+                return board
+            })
+            newState = {...state, boards: newBoards}
+            localStorage.state = JSON.stringify(newState)
+            return newState
         default:
             return state
     }
