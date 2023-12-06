ce363557abc69c4890040d8b83fba90a8240ebe4
Cheke <chekelosos@gmail.com>
2023-12-06 14:07:41 -0600

    Changed functionality of the hamburguer button so that when one of the options gets clicked it stop's displaying the options

--- src/components/Utilities/HamburgerButton.jsx
+++ src/components/Utilities/HamburgerButton.jsx
@@ -2,6 +2,20 @@ import { useState } from "react"
 
 export default function HamburgerButton({handleShowDeletePopUp, handleShowEditPopUp, type, active, showOptions, toggleShowOptions}){
 
+    function handleEditClick(){
+        if (active){
+            handleShowEditPopUp()
+            toggleShowOptions()
+        }
+    }
+
+    function handleDeleteClick(){
+        if(active){
+            handleShowDeletePopUp()
+            toggleShowOptions()
+        }
+    }
+
     return (
         <div className="relative">
             <button onClick={toggleShowOptions} className="flex flex-col gap-1 max-h-[20px]">
@@ -12,9 +26,9 @@ export default function HamburgerButton({handleShowDeletePopUp, handleShowEditPo
             {
                 showOptions ? 
                 <div className="absolute top-11 right-0 w-[192px] h-[94px] bg-white shadow rounded-3xl z-40">
-                    <div className="dark:bg-darkGrey  flex flex-col gap-4 w-[200px] p-4 bg-magenta rounded-xl">
-                        <button onClick={() => active ? handleShowEditPopUp(): ''} className={`dark:text-white text-left ${active ? '' : ' opacity-50 cursor-default'}`}>Edit {type}</button>
-                        <button onClick={() => active ? handleShowDeletePopUp() : ''} className={`text-left text-mainRed ${active ? '' : ' opacity-50 cursor-default'}`}>Delete {type}</button>
+                    <div className=" dark:bg-veryDarkGrey  flex flex-col gap-4 w-[200px] p-4 bg-magenta rounded-xl">
+                        <button onClick={handleEditClick} className={`dark:text-white text-left ${active ? '' : ' opacity-50 cursor-default'}`}>Edit {type}</button>
+                        <button onClick={handleDeleteClick} className={`text-left text-mainRed ${active ? '' : ' opacity-50 cursor-default'}`}>Delete {type}</button>
                     </div>  
                 </div>
                 : ''
