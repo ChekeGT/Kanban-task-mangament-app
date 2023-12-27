976ab4cf15771084ccd0d80930415aab8b07f07d
Cheke <chekelosos@gmail.com>
2023-12-26 22:10:43 -0600

    Added responsiveness for the board bar buttons. and added font awesome to the project.

--- src/components/BoardBar/AddNewTaskButton.jsx
+++ src/components/BoardBar/AddNewTaskButton.jsx
@@ -10,8 +10,8 @@ export default function AddNewTaskButton({showPopUp, active}){
         }
     }
     return (
-        <button onClick={hanldeClick} className={` bg-mainPurple text-white p-2 rounded-2xl h-[48px] w-[164px] text-center ${active ? 'opacity-100' : 'opacity-50 cursor-default' }`}>
-            + Add New Task
+        <button onClick={hanldeClick} className={`bg-mainPurple text-white p-2 rounded-2xl h-[48px] w-[164px] text-center ${active ? 'opacity-100' : 'opacity-50 cursor-default' }`}>
+            <p><i className="fa-solid fa-plus"></i> <span className="hidden md:inline-block">Add New Task</span></p>
         </button>
         )
 }
--- src/components/BoardBar/BoardBar.jsx
+++ src/components/BoardBar/BoardBar.jsx
@@ -52,9 +52,9 @@ export default function BoardBar({boards, theme, board, numberOfBoards}){
     return (
         <>
             <div className=" w-full flex flex-row items-center justify-between md:w-10/12 right-0 bg-white shadow  py-7 pl-2 pr-5 h-[96px] dark:bg-darkGrey dark:text-white absolute">
-                <div className="flex gap-3 items-center mr-3">
+                <div className="w-9/12 flex gap-4 items-center mr-3">
                     <svg className=" scale-125 min-w-[30px] md:hidden" width="24" height="25" xmlns="http://www.w3.org/2000/svg"><g fill="#635FC7" fill-rule="evenodd"><rect width="6" height="25" rx="2"/><rect opacity=".75" x="9" width="6" height="25" rx="2"/><rect opacity=".5" x="18" width="6" height="25" rx="2"/></g></svg>
-                    <p className="font-custom text-black font-bold text-sm md:text-3xl dark:text-white">{board == null ? numberOfBoards > 0 ? 'You have no boards selected.' : 'You have no boards' : board.name}</p>
+                    <p className="font-custom text-black font-bold text-xl md:text-3xl dark:text-white">{board == null ? numberOfBoards > 0 ? 'You have no boards selected.' : 'You have no boards' : board.name}</p>
                     <button onClick={toggleShowNavBar}>
                         {showNavBar ? <svg className=" scale-150 md:hidden" width="25" height="7" xmlns="http://www.w3.org/2000/svg"><path stroke="#635FC7" stroke-width="2" fill="none" d="m1 1 4 4 4-4"/></svg>
                         : <svg className="scale-150 md:hidden" width="25" height="7" xmlns="http://www.w3.org/2000/svg"><path stroke="#635FC7" stroke-width="2" fill="none" d="M9 6 5 2 1 6"/></svg>}
@@ -64,7 +64,7 @@ export default function BoardBar({boards, theme, board, numberOfBoards}){
                     </div>
                 </div>
                 
-                <div className="flex flex-row items-center gap-5">
+                <div className="w-3/12 md:w-auto flex flex-row items-center gap-5">
                      <AddNewTaskButton board={board} showPopUp={toggleShowAddPopUp} active={activeAddNewTaskButton}/>
                      <HamburgerButton active={activeHamburgerButton} showOptions={showOptions} toggleShowOptions={toggleShowOptions} type={'Board'} handleShowDeletePopUp={toggleShowDeletePopUp} handleShowEditPopUp={toggleShowEditPopUp}/>
                 </div>
